96ab16dc80a6a963758718e2264beff447607d6f
LempiRajala <lempirajala79@gmail.com>
2025-03-10 02:04:11 +0500

    fix footer style

--- web/src/components/footer.tsx
+++ web/src/components/footer.tsx
@@ -11,25 +11,28 @@ export async function Footer({
 	return (
 		<footer className={clsx(
 			"bg-background border-0 border-t-1 border-solid border-border py-1 px-2",
-			"font-geist text-text-secondary",
-			links.length ?
-				"flex justify-between items-center" :
-				"flex justify-center items-center",
 			className,
 		)}>
-			<p className="font-audiowide">© Tech&AI, {new Date().getFullYear()}</p>
-			{
-				links.map(({ href, text }, i) => (
-					<Link
-						key={i}
-						className="transition-colors hover:text-text"
-						href={href} 
-						prefetch={false}
-					>
-						{text}
-					</Link>
-				))
-			}
+			<div className={clsx(
+				"font-geist text-text-secondary max-w-laptop mx-auto",
+				links.length ?
+					"flex justify-between items-center" :
+					"flex justify-center items-center",
+			)}>
+				<p className="font-audiowide">© Tech&AI, {new Date().getFullYear()}</p>
+				{
+					links.map(({ href, text }, i) => (
+						<Link
+							key={i}
+							className="transition-colors hover:text-text"
+							href={href} 
+							prefetch={false}
+						>
+							{text}
+						</Link>
+					))
+				}
+			</div>
 		</footer>
 	)
 }
